5ea59c3c1871e8f8022b2a76da17568a5e57840f
David De Anda <omarandstuff@gmail.com>
2019-07-18 23:13:21 -0500

    Refactor normalize module

--- lib/normalizator/normalize.rb
+++ lib/normalizator/normalize.rb
@@ -32,50 +32,52 @@ module Normalizator
 
       @rules_keys.each do |field|
         if field.instance_of? Array
-          next if field.any? { |sub_field| !original_row.key?(sub_field) }
-
-          values = field.map { |sub_field| original_row[sub_field] }
-
-          normalized_values = []
-
-          if @rules[field].instance_of? Array
-            ruled_field = values
-
-            @rules[field].each do |rule|
-              ruled_field = rule.apply(ruled_field, original_row)
-            end
-
-            normalized_values = ruled_field
-          else
-            normalized_values = @rules[field].apply(values, original_row)
-          end
-
-          field.each_with_index do |sub_field, index|
-            new_row[sub_field] = normalized_values[index]
-          end
+          next if should_skip_mutly_field_rule(field, original_row)
+          run_rules_on_multy_value(field, new_row, original_row)
         else 
-          unless original_row.key? field
-            next if @options[:ignore_unmatched_rules]
-  
-            new_row[field] = @options[:default_unmatched_rules_value]
-            next
-          end
-          
-          if @rules[field].instance_of? Array
-            ruled_field = original_row[field]
-
-            @rules[field].each do |rule|
-              ruled_field = rule.apply(ruled_field, original_row)
-            end
-
-            new_row[field] = ruled_field
-          else
-            new_row[field] = @rules[field].apply(original_row[field], original_row)
-          end
+          next if should_skip_rule(field, new_row, original_row)
+          new_row[field] = run_rules_on_value(@rules[field], original_row[field], original_row)
         end
       end
 
       new_row
+    end
+
+    def run_rules_on_multy_value(multy_field, new_row, original_row)
+      values = multy_field.map { |sub_field| original_row[sub_field] }
+
+      normalized_values = run_rules_on_value(@rules[multy_field], values, original_row)
+
+      multy_field.each_with_index do |sub_field, index|
+        new_row[sub_field] = normalized_values[index]
+      end
+    end
+
+    def run_rules_on_value(rules, values, original_row)
+      if rules.instance_of? Array
+        ruled_field = values
+
+        rules.each do |rule|
+          ruled_field = rule.apply(ruled_field, original_row)
+        end
+
+        ruled_field
+      else
+        rules.apply(values, original_row)
+      end
+    end
+
+    def should_skip_rule(field, new_row, original_row)
+      unless original_row.key? field
+        return true if @options[:ignore_unmatched_rules]
+
+        new_row[field] = @options[:default_unmatched_rules_value]
+        true
+      end
+    end
+
+    def should_skip_mutly_field_rule(multy_field, original_row)
+      multy_field.any? { |sub_field| !original_row.key?(sub_field) }
     end
   end
 end
