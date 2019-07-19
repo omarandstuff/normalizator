92983142994d34148fb1e4d1284f4e3d3ce18f09
David De Anda <omarandstuff@gmail.com>
2019-07-18 23:57:07 -0500

    Refactor enum rule

--- lib/rules/enum_rule.rb
+++ lib/rules/enum_rule.rb
@@ -8,35 +8,41 @@ module Normalizator
       raise(RuleError, 'Array of enumerators is expected in the rule constructor options') unless @options[:enumerators].instance_of? Array
 
       @options[:enumerators].each do |enum|
-        if @options[:case_sensitive]
-          if value.strip == enum.strip
-            best_match = value
-            break
-          end
+        (best_match, perfect_match) = match_agains_enumerator(enum, value , best_match)
 
-          if @options[:diffuse]
-            if enum.include? value
-              best_match = enum
-            end
-          end
-        else
-          sanitized_value = value.downcase.strip
-          sanitized_enum = enum.downcase.strip
-
-          if sanitized_value == sanitized_enum
-            best_match = enum
-            break
-          end
-
-          if @options[:diffuse]
-            if sanitized_enum.include? sanitized_value
-              best_match = enum
-            end
-          end
-        end
+        break if perfect_match
       end
 
       best_match ? best_match : value_on_failure
+    end
+
+    private
+
+    def match_agains_enumerator(enum, value, best_match)
+      if @options[:case_sensitive]
+        (best_match, perfect_match) = case_sensitive_matching(enum, value, best_match)
+      else
+        (best_match, perfect_match) = case_insensitive_matching(enum, value, best_match)
+      end
+
+      [best_match, perfect_match]
+    end
+
+    def case_sensitive_matching(enum, value, best_match)
+      return [value, true] if value.strip == enum.strip
+      return [enum, false] if @options[:diffuse] && enum.include?(value)
+
+      [best_match, false]
+    end
+
+    def case_insensitive_matching(enum, value, best_match)
+      sanitized_value = value.downcase.strip
+      sanitized_enum = enum.downcase.strip
+
+      return [enum, true] if sanitized_value == sanitized_enum
+      return [enum, false] if @options[:diffuse] && sanitized_enum.include?(sanitized_value)
+
+      [best_match, false]
     end
   end
 end
